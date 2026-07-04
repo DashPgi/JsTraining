0402a33ca482884cd3865c9d7e6921a4e1f5f182
DashPgi <pajiparsa@gmail.com>
2026-07-04 13:40:02 +0330

    Temperture-Converter

--- app.js
+++ app.js
@@ -1,20 +1,77 @@
-function calculateTip() {
-  var Bill = Number(document.getElementById("bill-amount").value);
-  var Tip = Number(document.getElementById("tip-percentage").value);
-  var Resault = document.getElementById("tip-amount");
+function convertTemperature(type) {
 
-  if (Bill < 1) {
-    let Tipamount = "SooratHesab Kamtar az 1000 ast";
-    Resault.textContent = "total :" +Tipamount;
-  }
-  if (Tip < 1) {
-    let Tipamount = "Darsade Moshakhas shode Eshtebah Ast!";
-    Resault.textContent = "total :" +Tipamount;
-  } else if (Tip < 1 || Bill < 1) {
-    let Tipamount = "Voroodi Sahih Nist!";
-    Resault.textContent = "total :" + Tipamount;
-  } else {
-let Tipamount = Bill + (Bill * Tip / 100);
-    Resault.textContent = "total : " + Tipamount;
-  }
+    let celsiusInput = document.getElementById("celsius-input");
+    let fahrenheitInput = document.getElementById("fahrenheit-input");
+    let kelvinInput = document.getElementById("kelvin-input");
+
+    let celsius;
+    let fahrenheit;
+    let kelvin;
+
+    switch (type) {
+
+        case "celsius":
+
+            celsius = parseFloat(celsiusInput.value);
+
+            if (isNaN(celsius)) {
+                fahrenheitInput.value = "";
+                kelvinInput.value = "";
+                return;
+            }
+
+            fahrenheit = (celsius * 9 / 5) + 32;
+            kelvin = celsius + 273.15;
+
+            fahrenheitInput.value = fahrenheit.toFixed(2);
+            kelvinInput.value = kelvin.toFixed(2);
+
+            break;
+
+        case "fahrenheit":
+
+            fahrenheit = parseFloat(fahrenheitInput.value);
+
+            if (isNaN(fahrenheit)) {
+                celsiusInput.value = "";
+                kelvinInput.value = "";
+                return;
+            }
+
+            celsius = (fahrenheit - 32) * 5 / 9;
+            kelvin = celsius + 273.15;
+
+            celsiusInput.value = celsius.toFixed(2);
+            kelvinInput.value = kelvin.toFixed(2);
+
+            break;
+
+        case "kelvin":
+
+            kelvin = parseFloat(kelvinInput.value);
+
+            if (isNaN(kelvin)) {
+                celsiusInput.value = "";
+                fahrenheitInput.value = "";
+                return;
+            }
+
+            celsius = kelvin - 273.15;
+            fahrenheit = (celsius * 9 / 5) + 32;
+
+            celsiusInput.value = celsius.toFixed(2);
+            fahrenheitInput.value = fahrenheit.toFixed(2);
+
+            break;
+    }
+
+    document.body.classList.remove("freezing", "cold", "hot");
+
+    if (celsius < 0) {
+        document.body.classList.add("freezing");
+    } else if (celsius < 28) {
+        document.body.classList.add("cold");
+    } else {
+        document.body.classList.add("hot");
+    }
 }
